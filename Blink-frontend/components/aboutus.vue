806b1c774ea38db74afc52924cffa63b89e7190a
Ibrahim Alou Issa <ibrahimalouissa@gmail.com>
2022-05-08 14:05:18 +0200

    about us & footer changes

--- Blink-frontend/components/aboutus.vue
+++ Blink-frontend/components/aboutus.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="my-20">
     <img src="assets/img/tilttop.svg" alt="" class="rotate">
-    <div class="w-full py-20 bg-[#bbe5fd]">
-      <h1 class="m-8 text-4xl font-semibold">About <span class="font-bold text-blue-600">Us</span></h1>
-      <div class="m-8 text-lg">
-        <p>Our expert plumbers are the right team to call for both residential and commercial plumbing services. We provide the fastest and most reliable maintenance and repair solutions at no extra cost or overtime charge. Our plumbers are the most recognized professionals in the world because we’re committed to amazing service and nothing wears on homeowners like the idea of handling plumbing problems. </p>
+    <div class="w-full p-1 bg-[#bbe5fd]">
+      <div class="m-8 lg:m-16 xl:m-24 bg-white shadow-xl rounded-lg p-4">
+        <h1 class="m-8 text-4xl font-semibold">About <span class="font-bold text-blue-600">Us</span></h1>
+        <div class="m-8 text-lg">
+          <p>Our expert plumbers are the right team to call for both residential and commercial plumbing services. We provide the fastest and most reliable maintenance and repair solutions at no extra cost or overtime charge. Our plumbers are the most recognized professionals in the world because we’re committed to amazing service and nothing wears on homeowners like the idea of handling plumbing problems. </p>
+        </div>
       </div>
     </div>
     <img src="assets/img/tilttop.svg" alt="">
