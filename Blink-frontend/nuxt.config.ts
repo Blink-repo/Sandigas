211bf42790240ab86bbeb1f399a939bb7f5abecf
unknown <ibrahimalouissa@gmail.com>
2022-05-10 12:13:23 +0200

    deleted flowbite Error on production level

--- Blink-frontend/nuxt.config.ts
+++ Blink-frontend/nuxt.config.ts
@@ -3,7 +3,6 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    plugins: ['@/plugins/flowbite.client.ts'],
     css: ["@/assets/styles/styles.css"],
     build: {
         postcss: {
