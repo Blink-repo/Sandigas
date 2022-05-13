cf35c1b2b1e666257e3f28cd25baf00fb1ee7cd6
unknown <ibrahimalouissa@gmail.com>
2022-05-13 13:46:51 +0200

    some small touch ups

--- Blink-frontend/components/home.vue
+++ Blink-frontend/components/home.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="landing-bg">
-    <navbar class="w-full "/>
+    <navbar class="w-full"/>
     <div class="flex m-8 sm:m-24 xl:mx-48 grid grid-cols-1 lg:grid-cols-2">
       <div>
-        <h1 class="text-4xl sm:text-5xl 2xl:text-8xl font-semibold m-2">Sanitair, verwarming &<br><span class="text-secondary">herstellingen</span></h1>
+        <h1 class="text-4xl sm:text-5xl lg:text-6xl xl:text-7xl font-semibold m-2">Sanitair, verwarming &<br><span class="text-secondary">herstellingen</span></h1>
         <p class="m-2 text-md sm:text-lg 2xl:text-xl"> Wij helpen mensen die hun lekkage willen herstellen, een cv-ketel willen laten installeren of sanitair willen laten aanleggen met onze professionele kennis en expertise. Zodat u zich geen zorgen meer moet maken over dat vervelende probleem. Wij proberen iedereen zo snel mogenlijk verder te helpen zodat u niet te lang moet wachten.
         </p>
         <a href="#contactform">
-          <button type="button" class="my-6 px-5 py-3 text-base font-medium text-center text-white bg-secondary rounded-lg shadow-xl hover:bg-secondary-hover focus:ring-4 focus:outline-none focus:ring-blue-300">Contacteer ons</button>
+          <button type="button" class="m-2 my-6 px-5 py-3 text-base font-medium text-center text-white bg-secondary rounded-lg shadow-xl hover:bg-secondary-hover focus:ring-4 focus:outline-none focus:ring-secondary">Contacteer ons</button>
         </a>
       </div>
       <div class="hero container max-w-screen-lg mx-auto flex justify-center">
--- Blink-frontend/components/navbar.vue
+++ Blink-frontend/components/navbar.vue
@@ -4,8 +4,8 @@
            <img src="/img/logos/logo.png" class="w-24 lg:w-32 xs:m-1 sm:m-2 md:m-3" alt="logo" />
 
           <div class="flex md:order-2">
-        <a href="tel:+32484794493"><button type="button" class="sm:hidden text-white bg-secondary hover:bg-secondary-hover focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0">Bel ons</button></a>
-        <button @click="open = !open" data-collapse-toggle="mobile-menu-4" type="button" class="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200" aria-controls="mobile-menu-4" aria-expanded="false">
+        <a href="tel:+32484794493"><button type="button" class="sm:hidden text-white bg-secondary hover:bg-secondary-hover focus:ring-4 focus:outline-none focus:ring-secondary font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0">Bel ons</button></a>
+        <button @click="open = !open" data-collapse-toggle="mobile-menu-4" type="button" class="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-secondary" aria-controls="mobile-menu-4" aria-expanded="false">
           <span class="sr-only">Open main menu</span>
           <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
           <svg class="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
