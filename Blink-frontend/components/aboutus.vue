fe9d591893aea6e2101a31a954dba1ecdd9999a9
mo <mohammed.haibout@me.com>
2022-05-13 15:02:16 +0200

    mini micro change about us

--- Blink-frontend/components/aboutus.vue
+++ Blink-frontend/components/aboutus.vue
@@ -5,7 +5,7 @@
       <div class="m-4 lg:m-16 xl:m-24 bg-white shadow-xl rounded-lg p-4">
         <h1 class="m-6 text-4xl font-semibold">Over <span class="font-bold text-secondary">Ons</span></h1>
         <div class="m-6 text-lg">
-          <p>Wij zijn een gedreven, jong koppel die een loodgietersbedrijf gestart hebben genaamd Sandigas. Ons doel is om u te voorzien van het nodige comfort inzaken sanitair en verwarming. Met een grote passie komen wij uw probleem oplossen.</p>
+          <p>Wij zijn een gedreven, jong koppel die een loodgietersbedrijf gestart zijn genaamd Sandigas. Ons doel is om u te voorzien van het nodige comfort inzaken sanitair en verwarming. Met een grote passie komen wij uw probleem oplossen.</p>
         </div>
       </div>
     </div>
